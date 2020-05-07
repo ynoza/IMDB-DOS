a4f7dbb9596d196dc46ff4ece76ab004c55df963
ynoza <46010696+ynoza@users.noreply.github.com>
2020-05-06 20:21:11 -0400

    Add files via upload

---
+++ util.py
@@ -0,0 +1,38 @@
+class Node():
+    def __init__(self, movie_id, parent, person_id):
+        self.movie_id = movie_id
+        self.parent = parent
+        self.person_id = person_id
+
+
+class StackFrontier():
+    def __init__(self):
+        self.frontier = []
+
+    def add(self, node):
+        self.frontier.append(node)
+
+    def contains_state(self, person_id):
+        return any(node.person_id == person_id for node in self.frontier)
+
+    def empty(self):
+        return len(self.frontier) == 0
+
+    def remove(self):
+        if self.empty():
+            raise Exception("empty frontier")
+        else:
+            node = self.frontier[-1]
+            self.frontier = self.frontier[:-1]
+            return node
+
+
+class QueueFrontier(StackFrontier):
+
+    def remove(self):
+        if self.empty():
+            raise Exception("empty frontier")
+        else:
+            node = self.frontier[0]
+            self.frontier = self.frontier[1:]
+            return node
